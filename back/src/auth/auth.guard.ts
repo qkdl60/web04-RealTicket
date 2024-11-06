8b22b40908f66627d1378efb2436ce539168b36f
KWAKMANBO <kwak98hs@gmail.com>
2024-11-07 02:15:48 +0900

    💩 chore   : 작업하던 내용 Commit

---
+++ back/src/auth/auth.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  private validateRequest(request: any) {
+    //TODO
+    // Session에 request의 Cookie가 있다면 True
+    // 없다면 False를 반환하도록
+    return true;
+  }
+}
