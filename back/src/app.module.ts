d5d4636a8278958ac7a6af2559f65441079253ef
DongHyun <kimdh0315@naver.com>
2024-11-07 17:07:07 +0900

    ✨ feat: MySQL, TypeOrm 설정
    - MySQL, TypeORM 패키지 설치
    - 환경 변수를 통한 MySQL 연결
    Issue Resolved: #

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from './config/typeOrmConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/config/typeOrmConfig.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+const ormConfig: TypeOrmModuleOptions = {
+    type: process.env.DATABASE_TYPE as 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true'
+};
+
+export default ormConfig;
