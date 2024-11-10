d4e3ce7477b2d87b376f0b7691fd95b22b623fe9
nocheol <qkdl60@gmail.com>
2024-11-11 03:24:50 +0900

    ✨ feat:리액트라우터 추가 및 적용
    - 페이지 전환을 위한 react router 설치
    - 간단하게 기본 페이지 라우팅 설정
    Issue Resolved: #12

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,9 @@
-import { useState } from 'react';
+import { RouterProvider } from 'react-router-dom';
+
+import router from '@/routes/index.tsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const plus = () => {
-    setCount(count + 1);
-  };
-  return (
-    <>
-      <div className="text-rose-200 flex w-2 h-5">{count}</div>
-      <button onClick={plus} className="border-2 border-pink-100  border-spacing-36">
-        click me!!
-      </button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
