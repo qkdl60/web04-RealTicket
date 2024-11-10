cb6247a4637529a7ef8768f2e5f9a7da3742043f
nocheol <qkdl60@gmail.com>
2024-11-10 16:45:36 +0900

    💩 chore:불필요한 파일, asset 삭제
    Issue Resolved: #

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,17 @@
 import { useState } from 'react';
 
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 function App() {
   const [count, setCount] = useState(0);
 
+  const plus = () => {
+    setCount(count + 1);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div className="text-rose-200 flex w-2 h-5">{count}</div>
+      <button onClick={plus} className="border-2 border-pink-100  border-spacing-36">
+        click me!!
+      </button>
     </>
   );
 }
