23ff84eae7a9e6ebdfe6fc924eee7df020aaa683
신성규 <kxu4583@naver.com>
2024-11-12 22:48:17 +0900

    🐛 fix: app.module 내 controllers, providers에서 불필요한 하위 모듈 요소 제거
    Issue Resolved: #48

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -7,18 +7,9 @@ import { AppService } from './app.service';
 import redisConfig from './config/redisConfig';
 import ormConfig from './config/typeOrmConfig';
 import { PlaceModule } from './place/place.module';
-import { PlaceService } from './place/place.service';
-import { ProgramController } from './program/program.controller';
 import { ProgramModule } from './program/program.module';
-import { ProgramService } from './program/program.service';
-import { ReservationController } from './reservation/reservation.controller';
 import { ReservationModule } from './reservation/reservation.module';
-import { ReservationService } from './reservation/reservation.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UtilController } from './util/util.controller';
 import { UtilModule } from './util/util.module';
-import { UtilService } from './util/util.service';
 
 @Module({
   imports: [
@@ -29,7 +20,7 @@ import { UtilService } from './util/util.service';
     PlaceModule,
     UtilModule,
   ],
-  controllers: [AppController, UserController, ProgramController, ReservationController, UtilController],
-  providers: [AppService, UserService, ProgramService, ReservationService, PlaceService, UtilService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
