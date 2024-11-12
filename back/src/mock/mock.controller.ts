b832bb081c5a50cd371569f0f171e7b404ed0bee
ShinSungkyu <kxu4583@naver.com>
2024-11-13 00:54:48 +0900

    ✨ feat: `mock` 모듈 기본 구조 생성
    Issue Resolved: #

---
+++ back/src/mock/mock.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+
+import { MockService } from './mock.service';
+
+@Controller('mock')
+export class MockController {
+  constructor(private readonly mockService: MockService) {}
+}
