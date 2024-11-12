c02f424f12061f1301c519314921e24823c5c3e6
신성규 <kxu4583@naver.com>
2024-11-12 22:16:24 +0900

    ✨ feat: `reservation` 모듈 기본 구조 생성
    Issue Resolved: #

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -9,12 +9,20 @@ import ormConfig from './config/typeOrmConfig';
 import { ProgramController } from './program/program.controller';
 import { ProgramModule } from './program/program.module';
 import { ProgramService } from './program/program.service';
+import { ReservationController } from './reservation/reservation.controller';
+import { ReservationModule } from './reservation/reservation.module';
+import { ReservationService } from './reservation/reservation.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), RedisModule.forRoot(redisConfig), ProgramModule],
-  controllers: [AppController, UserController, ProgramController],
-  providers: [AppService, UserService, ProgramService],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    RedisModule.forRoot(redisConfig),
+    ProgramModule,
+    ReservationModule,
+  ],
+  controllers: [AppController, UserController, ProgramController, ReservationController],
+  providers: [AppService, UserService, ProgramService, ReservationService],
 })
 export class AppModule {}
