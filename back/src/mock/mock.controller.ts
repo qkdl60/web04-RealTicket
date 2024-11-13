aa6a98b8b7b449d7b009553cd5bb90609b95005c
ShinSungkyu <kxu4583@naver.com>
2024-11-13 23:16:18 +0900

    🐛 fix: mock 모듈 내 불필요 요소 삭제
    - mock 모듈 내 하위 서비스들은 테스트 파일의 의미가 미미하므로 삭제함.
    - mock 모듈의 서비스, 컨트롤러의 역할이 없으므로 삭제함.
    
    Issue Resolved: #

--- back/src/mock/mock.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-import { MockService } from './mock.service';
-
-@Controller('mock')
-export class MockController {
-  constructor(private readonly mockService: MockService) {}
-}
--- back/src/mock/mock.module.ts
+++ back/src/mock/mock.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { BookingModule } from './booking/booking.module';
 import { EventModule } from './event/event.module';
-import { MockController } from './mock.controller';
-import { MockService } from './mock.service';
 import { PlaceModule } from './place/place.module';
 import { ProgramModule } from './program/program.module';
 import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  controllers: [MockController],
-  providers: [MockService],
   imports: [ReservationModule, BookingModule, PlaceModule, EventModule, ProgramModule],
 })
 export class MockModule {}
