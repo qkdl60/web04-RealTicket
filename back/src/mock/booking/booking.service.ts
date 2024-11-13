139fd39767c40eaa71aac93e4ba436d88d657899
ShinSungkyu <kxu4583@naver.com>
2024-11-13 19:13:48 +0900

    ✨ feat: 대기 큐에서 재입장 여부 수신(SSE) 목 API 구현
    Issue Resolved: #

--- back/src/mock/booking/booking.service.ts
+++ back/src/mock/booking/booking.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { interval, Observable, takeWhile } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class BookingService {}
+export class BookingService {
+  rePermission(eventId: number): Observable<any> {
+    return interval(1000).pipe(
+      map((num) => {
+        const userOrder = 100 - num * 10;
+        const totalWaiting = 100 + num * 13;
+        const restMillisecond = 10000 - num * 1000;
+        const enteringStatus = userOrder <= 0;
+
+        return {
+          data: {
+            id: eventId,
+            data: {
+              'user-order': userOrder,
+              'total-waiting': totalWaiting,
+              'rest-millisecond': restMillisecond,
+              'entering-status': enteringStatus,
+            },
+          },
+        };
+      }),
+
+      takeWhile((response) => {
+        return !response.data.data['entering-status'];
+      }, true),
+    );
+  }
+}
