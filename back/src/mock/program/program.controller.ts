9f2a666faee0c36a9ca6513f824f14c79d99073b
ShinSungkyu <kxu4583@naver.com>
2024-11-13 23:05:54 +0900

    ✨ feat: 프로그램 목록 조회 목 API 구현
    Path: mock/programs [GET]
    
    Issue Resolved: #

--- back/src/mock/program/program.controller.ts
+++ back/src/mock/program/program.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { ProgramService } from './program.service';
 
 @Controller('mock/programs')
 export class ProgramController {
   constructor(private readonly programService: ProgramService) {}
+
+  @Get()
+  getPrograms() {
+    return this.programService.getPrograms();
+  }
 }
--- back/src/mock/program/program.service.ts
+++ back/src/mock/program/program.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProgramService {}
+export class ProgramService {
+  getPrograms() {
+    return {
+      programs: [
+        {
+          id: 1,
+          name: '맘마미아',
+          genre: '연극',
+          place: {
+            id: 1,
+            name: '대극장',
+          },
+          'profile-url': null,
+        },
+      ],
+    };
+  }
+}
