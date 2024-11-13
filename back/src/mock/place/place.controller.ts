02e106841b7f039b1229d2a0952cb3d03017e886
ShinSungkyu <kxu4583@naver.com>
2024-11-13 22:45:43 +0900

    ✨ feat: 초기 좌석 배치 데이터 조회 목 API 구현
    - Path : mock/place/seats/:place-id [GET]
    - 지속적 업데이트 되는 좌석이 아니고, 가장 처음 좌석 배치 구조를 알기 위해 호출하는 API임.
    
    Issue Resolved: #

--- back/src/mock/place/place.controller.ts
+++ back/src/mock/place/place.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { PlaceService } from './place.service';
 
 @Controller('mock/place')
 export class PlaceController {
   constructor(private readonly placeService: PlaceService) {}
+
+  @Get('seats/:placeId')
+  async getSeatsByPlaceId() {
+    return await this.placeService.getSeatsByPlaceId();
+  }
 }
