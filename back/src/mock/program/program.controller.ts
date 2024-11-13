8c42569f9a1c4a2f6283beb2f53a86b21c826e24
ShinSungkyu <kxu4583@naver.com>
2024-11-13 23:09:06 +0900

    ✨ feat: 특정 프로그램 조회 목 API 구현
    Path : mock/programs/:program-id [GET]
    
    Issue Resolved: #

--- back/src/mock/program/program.controller.ts
+++ back/src/mock/program/program.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { ProgramService } from './program.service';
 
@@ -9,5 +9,10 @@ export class ProgramController {
   @Get()
   getPrograms() {
     return this.programService.getPrograms();
+  }
+
+  @Get(':programId')
+  getProgramById(@Param('programId') programId: number) {
+    return this.programService.getProgramById(programId);
   }
 }
--- back/src/mock/program/program.service.ts
+++ back/src/mock/program/program.service.ts
@@ -18,4 +18,30 @@ export class ProgramService {
       ],
     };
   }
+
+  getProgramById(programId: number) {
+    return {
+      id: programId,
+      name: '맘마미아',
+      'running-time': 1234,
+      genre: '연극',
+      actors: '김동현, 김동현',
+      place: {
+        id: 1,
+        name: '대극장',
+      },
+      'profile-url': null,
+      price: 15000,
+      events: [
+        {
+          id: 1,
+          'running-date': '2024-11-01 09:00',
+        },
+        {
+          id: 2,
+          'running-date': '2024-11-01 12:00',
+        },
+      ],
+    };
+  }
 }
