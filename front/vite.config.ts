9af23a7878227f16332b1099561d769eb2946652
nocheol <qkdl60@gmail.com>
2024-11-14 17:06:10 +0900

    💩 chore:vite 설정 변경
    Issue Resolved: #

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
