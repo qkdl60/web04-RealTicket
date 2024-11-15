5199fd2162a0eea2685ff53ca23ad9b831a2418b
KWAKMANBO <kwak98hs@gmail.com>
2024-11-16 05:12:12 +0900

    ✨ feat: winstonLogger 적용
    - 프로젝트 전역적으로 winston 로그 적용
    - 로그파일 생성 기능 추가
    Issue Resolved: #

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,12 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+
+import './config/setEnviorment';
 
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/setupSwagger';
+import { winstonLoggerConfig } from './util/winstonlogger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLoggerConfig,
+  });
   setupSwagger(app);
   app.useGlobalPipes(
     new ValidationPipe({
