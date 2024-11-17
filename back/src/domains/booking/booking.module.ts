3d4bb29da4ed020f11dc5a23ca2726149f79b844
ShinSungkyu <kxu4583@naver.com>
2024-11-17 13:20:33 +0900

    🐛 fix: booking 모듈에 컨트롤러 누락 수정
    Issue Resolved: #

--- back/src/domains/booking/booking.module.ts
+++ back/src/domains/booking/booking.module.ts
@@ -1,14 +1,15 @@
-// booking.module.ts
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../../auth/auth.module';
-import { EventModule } from '../event/event.module'; // Import EventModule
+import { EventModule } from '../event/event.module';
 
+import { BookingController } from './controller/booking.controller';
 import { BookingService } from './service/booking.service';
 import { InBookingService } from './service/in-booking.service';
 
 @Module({
   imports: [EventModule, AuthModule],
+  controllers: [BookingController],
   providers: [BookingService, InBookingService],
 })
 export class BookingModule {}
