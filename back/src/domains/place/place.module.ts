c19d8f04e2d4498c42d926eed95e660ae40c3b3c
DongHyun <kimdh0315@naver.com>
2024-11-17 21:19:49 +0900

    ✨ feat: 장소 레포지토리 레이어 생성
    - 장소 레포지토리 생성
    - 장소 조회하는 레포지토리 메소드 생
    Issue Resolved: #86

--- back/src/domains/place/place.module.ts
+++ back/src/domains/place/place.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Place } from './entity/place.entity';
+import { PlaceRepository } from './repository/place.repository';
 import { PlaceService } from './service/place.service';
 
 @Module({
-  providers: [PlaceService],
+  imports: [TypeOrmModule.forFeature([Place])],
+  providers: [PlaceService, PlaceRepository],
+  exports: [PlaceRepository],
 })
 export class PlaceModule {}
