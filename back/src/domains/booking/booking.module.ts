29cd40e2fda2dd6f5619e74bcd1597faed95f336
ShinSungkyu <kxu4583@naver.com>
2024-11-20 17:49:56 +0900

    ✨ feat: 실시간 좌석 관리 서비스 구현
    - 좌석 예약, 취소 메서드 구현
    - 좌석 현황 구독(SSE) 메서드 구현
    
    Issue Resolved: #

--- back/src/domains/booking/booking.module.ts
+++ back/src/domains/booking/booking.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../../auth/auth.module';
 import { EventModule } from '../event/event.module';
+import { PlaceModule } from '../place/place.module';
 
 import { BookingController } from './controller/booking.controller';
+import { BookingSeatsService } from './service/booking-seats.service';
 import { BookingService } from './service/booking.service';
 import { InBookingService } from './service/in-booking.service';
 
 @Module({
-  imports: [EventModule, AuthModule],
+  imports: [EventModule, AuthModule, PlaceModule],
   controllers: [BookingController],
-  providers: [BookingService, InBookingService],
+  providers: [BookingService, InBookingService, BookingSeatsService],
 })
 export class BookingModule {}
