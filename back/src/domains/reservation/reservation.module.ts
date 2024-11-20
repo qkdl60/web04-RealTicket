3c02fdffdd710c5276b89708e4d79cde42f7c3dd
KWAKMANBO <kwak98hs@gmail.com>
2024-11-20 20:48:14 +0900

    ✨ feat: Repository layer에서 DB에 저장하는 함수 추가
    -  ReservationRepository에서 예약정보를 저장하는 함수 storeReservation 함수 추가
    - ReservedSeatRepository 추가
    - ReservedSeatRepository에서 예약 좌성 정보를 저장하는 storeReservedSeat함수 추가
    
    Issue Resolved: #

--- back/src/domains/reservation/reservation.module.ts
+++ back/src/domains/reservation/reservation.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { EventModule } from '../event/event.module';
+import { UserModule } from '../user/user.module';
+
 import { ReservationController } from './controller/reservation.controller';
 import { Reservation } from './entity/reservation.entity';
 import { ReservedSeat } from './entity/reservedSeat.entity';
 import { ReservationRepository } from './repository/reservation.repository';
+import { ReservedSeatRepository } from './repository/reservedSeat.repository';
 import { ReservationService } from './service/reservation.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation, ReservedSeat])],
+  imports: [TypeOrmModule.forFeature([Reservation, ReservedSeat]), EventModule, UserModule],
   controllers: [ReservationController],
-  providers: [ReservationService, ReservationRepository],
+  providers: [ReservationService, ReservationRepository, ReservedSeatRepository],
 })
 export class ReservationModule {}
