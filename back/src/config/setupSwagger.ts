1d3fbe02b947090d9c81068787095287d6be18cf
DongHyun <kimdh0315@naver.com>
2024-11-21 15:13:24 +0900

    ✨ feat: swagger-api 인증 및 쿠키 유지
    - swagger 페이지에서 인증 기능 추가
    - 인증 시에 입력한 쿠키 값을 SID로 설정해서 전송
    Issue Resolved: #131

--- back/src/config/setupSwagger.ts
+++ back/src/config/setupSwagger.ts
@@ -6,8 +6,11 @@ export function setupSwagger(app: INestApplication): void {
     .setTitle('RealTicket API Docs')
     .setDescription('RealTicket의 API 명세시 입니다.')
     .setVersion('1.0.0')
+    .addCookieAuth('SID')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
 }
