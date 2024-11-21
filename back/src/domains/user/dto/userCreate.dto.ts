58a43e8be32522e4290e50cac81247df61dce726
KWAKMANBO <kwak98hs@gmail.com>
2024-11-21 12:43:28 +0900

    ♻️ refactor: 로그인, 회원가입 DTO길이 제한 설정
    - UserCreateDto, UserLoginDto에 아이디/비밀번호 최소, 최대 길이 설정 추가
    - UserCreateDto, UserLoginDto에 소문자와 숫자로만 이루어지도록 정규표현식 추가
    Issue Resolved: #

--- back/src/domains/user/dto/userCreate.dto.ts
+++ back/src/domains/user/dto/userCreate.dto.ts
@@ -1,15 +1,15 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
-
-import { NotIn } from '../../../decorator/not-in';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class UserCreateDto {
   @IsString()
-  @MinLength(1)
-  @MaxLength(15)
+  @MinLength(4)
+  @MaxLength(12)
+  @Matches(/^[a-z0-9]+$/)
   readonly loginId: string;
 
   @IsString()
-  @NotIn('loginId', { message: '비밀번호는 사용자 아이디를 포함할 수 없습니다.' })
-  //@Matches(/^[A-Za-z\d!@#$%^&*()]{8,12}/)
+  @MinLength(4)
+  @MaxLength(12)
+  @Matches(/^[a-z0-9]+$/)
   readonly loginPassword: string;
 }
