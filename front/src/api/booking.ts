26ffd9d9cd3c15b57224f9291587eccbbc973bbf
nocheol <qkdl60@gmail.com>
2024-11-26 15:39:01 +0900

    ♻️ refactor: api 상수 추가, 컨벤션 통일
    Issue Resolved: #

--- front/src/api/booking.ts
+++ front/src/api/booking.ts
@@ -11,18 +11,11 @@ export const getPermission = (id: number) => async () => {
   return apiClient.get(API.BOOKING.GET_PERMISSION(id)).then((res) => res.data);
 };
 
-export const postSeatCount = async (count: number) => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('');
-    }, 5000);
-  });
-  return apiClient.post(API.BOOKING.POST_COUNT, { bookingAmount: count });
-};
-
+export const postSeatCount = async (count: number) =>
+  apiClient.post(API.BOOKING.POST_COUNT, { bookingAmount: count });
 export const postSeat = (data: PostSeatData) => apiClient.post(API.BOOKING.POST_SEAT, data);
 
-interface PostSeatData {
+export interface PostSeatData {
   eventId: number;
   sectionIndex: number;
   seatIndex: number;
