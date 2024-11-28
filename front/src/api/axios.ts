406dc24ac709cbcde42cf324d5a9bf16973df43f
nocheol <qkdl60@gmail.com>
2024-11-28 19:40:54 +0900

    ♻️ refactor:전역 에러 핸들링 추가
    Issue Resolved: #

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -24,26 +24,29 @@ export const apiClient = axios.create({
 });
 
 const EXCLUDING_AUTH_ERROR_REDIRECT_URL_LIST = ['/user'];
-const UN_AUTHENTICATED_ERROR_STATUS = 403;
-
-const canRedirect = (error: AxiosError) => {
+const UN_AUTHENTICATION_ERROR_STATUS = 403;
+const UN_AUTHORIZATION_ERROR_STATUS = 401;
+const isAuthenticateError = (error: AxiosError) => {
   if (
-    error.status === UN_AUTHENTICATED_ERROR_STATUS &&
+    error.status === UN_AUTHENTICATION_ERROR_STATUS &&
     error.config?.url &&
     !EXCLUDING_AUTH_ERROR_REDIRECT_URL_LIST.includes(error.config.url)
   )
     return true;
   return false;
 };
-const isExclude = (error: AxiosError) => {
-  if (
-    error.status === UN_AUTHENTICATED_ERROR_STATUS &&
-    error.config?.url &&
-    EXCLUDING_AUTH_ERROR_REDIRECT_URL_LIST.includes(error.config.url)
-  ) {
+
+const isAuthorizationError = (error: AxiosError) => {
+  if (error.status === UN_AUTHORIZATION_ERROR_STATUS) {
     return true;
   }
+  return false;
+};
 
+const isServerError = (error: AxiosError) => {
+  if (error.status && error.status >= 500 && error.status < 600) {
+    return true;
+  }
   return false;
 };
 
@@ -53,14 +56,17 @@ apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
     if (isError(error)) {
-      if (canRedirect(error)) {
+      if (isAuthenticateError(error)) {
         toast.error('로그인이 필요합니다.\n로그인 후 이용해주세요.');
         auth.logout();
         router.navigate(ROUTE_URL.USER.LOGIN, { replace: true });
-        return Promise.reject(error);
       }
-      if (isExclude(error)) {
-        return Promise.resolve({ data: null });
+      if (isAuthorizationError(error)) {
+        toast.error('잘못된 접근입니다.\n다시 시도해주세요.');
+        router.navigate('/', { replace: true });
+      }
+      if (isServerError(error)) {
+        toast.error('서버에 문제가 있습니다.\n잠시 후 다시 시도해주세요.');
       }
       throw error;
     }
