f8cd3d551316bb7ca4ef43856c4f877bb3165bb1
nocheol <qkdl60@gmail.com>
2024-12-02 06:29:17 +0900

    ♻️ refactor: 렌더링 함수 위치 변경
    Issue Resolved: #

--- front/src/components/loaders/WithLogin.tsx
+++ front/src/components/loaders/WithLogin.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'react';
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { useAuthContext } from '@/hooks/useAuthContext.tsx';
@@ -10,8 +11,11 @@ import { ROUTE_URL } from '@/constants/index.ts';
 //TODO toast 알림 추가 필ㄹ요
 export default function WithLogin({ children }: PropsWithChildren) {
   const { isLogin } = useAuthContext();
+  useEffect(() => {
+    if (!isLogin) toast.warning('로그인이 필요한 서비스입니다.\n로그인 후 이용해주세요.');
+  }, [isLogin]);
+
   if (!isLogin) {
-    toast.warning('로그인이 필요한 서비스입니다.\n로그인 후 이용해주세요.');
     return <Navigate to={ROUTE_URL.USER.LOGIN} />;
   }
   return <>{children}</>;
