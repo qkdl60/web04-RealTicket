5e6755e2e10d3400e329160bd1aa81c6f00674c2
nocheol <qkdl60@gmail.com>
2025-03-31 02:21:41 +0900

    ✨ feat: msw 추가 및 init
    Issue Resolved: #

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { worker } from '@/mocks/browser.ts';
+
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function initMockWorker() {
+  if (import.meta.env.DEV) return worker.start();
+  return;
+}
+
+initMockWorker().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
